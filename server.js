240e80e846e581714c3b7a95b75ba23491964f3e
Abhishekabi <abimano99@gmail.com>
2019-09-13 22:16:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(PORT, () => console.log(`server running at port ${PORT}...`));
